c2446a12437d06569661906b8d0aaca081b44084
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-14 17:31:23 +0500

    bug fix

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {AppRegistry} from 'react-native';
 //import App from './App';
 
 import {name as appName} from './app.json';
-import NetflixCard from './src/components/NetflixCard';
+import NetflixCard from './src/screens/NetflixCard';
 
 AppRegistry.registerComponent(appName, () => NetflixCard);
