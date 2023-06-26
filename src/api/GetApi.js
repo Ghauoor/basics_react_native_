2d843407dfa8eb8fc31efa0a94bbfe8592ad997a
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-26 11:45:50 +0500

    Get Api

---
+++ src/api/GetApi.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
+import { Card, Paragraph } from 'react-native-paper';
+
+const GetApi = () => {
+  const [Data, setData] = useState([]);
+  const API = 'https://jsonplaceholder.typicode.com/posts';
+  const fetchPost = () => {
+    fetch(API)
+      .then(res => res.json())
+      .then(res => {
+        setData(res);
+      });
+  };
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  const renderItem = ({item: post}) => (
+    <Card style={styles.cardbox}>
+      <Text style={styles.title}>{post.title}</Text>
+      <Paragraph>{post.body}</Paragraph>
+    </Card>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={Data}
+        renderItem={renderItem}
+        keyExtractor={Data => Data.id}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 10,
+    marginVertical: 15,
+  },
+  cardbox: {
+    marginVertical: 8,
+    marginHorizontal: 5,
+    padding: 10,
+  },
+  title: {
+    backgroundColor: '#e2e2e2',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    borderRadius: 10,
+    paddingVertical: 5,
+  },
+});
+
+export default GetApi;
